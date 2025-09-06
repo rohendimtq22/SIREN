b743f220c6e320f8b2e3dbb9a714d419cd2da944
AFRcloud <rohendimtq246@gmail.com>
2025-09-06 21:25:44 +0700

    Update link.js

--- web/js/link.js
+++ web/js/link.js
@@ -11,7 +11,7 @@ let filteredProxyList = []
 let selectedProxy = null
 const defaultProxyUrl = "https://raw.githubusercontent.com/AFRcloud/ProxyList/refs/heads/main/ProxyList.txt"
 
-const serverDomains = ["stbwrt.web.id"]
+const serverDomains = [""]
 let selectedServerDomain = serverDomains[0] // Default to first domain
 const defaultUUID = "bbbbbbbb-cccc-4ddd-eeee-ffffffffffff"
 const itemsPerPage = 10
